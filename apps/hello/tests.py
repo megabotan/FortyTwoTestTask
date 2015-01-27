62dcf4327aee29b7416f4a0852f73bd55ed08de1
adre <adrekoval@gmail.com>
2015-01-27 17:34:02 +0200

    ticket#1 Added test

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,6 +1,23 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
-class SomeTests(TestCase):
-    def test_math(self):
-        assert(2+2==5)
+from hello.models import Person
+
+
+class HttpTest(TestCase):
+    def setUp(self):
+        self.person = Person.objects.all()[0]
+
+    def test_home(self):
+        response = self.client.get(reverse('hello.views.index'))
+        self.assertContains(response, self.me.name)
+        self.assertContains(response, self.me.last_name)
+        self.assertContains(response,
+            self.me.date_of_birth.strftime('%B %d, %Y'))
+        for string in self.me.bio.splitlines():
+            self.assertContains(response, string)
+        self.assertContains(response, self.me.email)
+        self.assertContains(response, self.me.jabber)
+        self.assertContains(response, self.me.skype)
+        for string in self.me.other_contacts.splitlines():
+            self.assertContains(response, string)
