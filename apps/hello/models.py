189da01e69815cb4dffc5e9de1dfce300dd733e7
adre <adrekoval@gmail.com>
2015-01-27 17:13:18 +0200

    ticket#1 Added person model and fixture

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    bio = models.TextField()
+    email = models.EmailField()
+    jabber = models.CharField(max_length=200)
+    skype = models.CharField(max_length=200)
+    other_contacts = models.TextField()
